8b257cdd1b2babe41d5dec1e83b090f91e022631
EliasMN <elias010493@gmail.com>
2024-04-09 18:44:33 +0200

    Clase 3

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterLinkWithHref } from '@angular/router';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Recipe } from '../../models/recipe';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLinkWithHref],
+  imports: [RouterLinkWithHref, FormsModule, ReactiveFormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  addRecipe = new FormGroup({
+    recipeName: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    recipeDifficulty: new FormControl('', [Validators.required, Validators.min(1), Validators.max(10)])
+  });
+
+
+  submit() {
+
+    let newRecipe: Recipe = {
+      name: this.addRecipe.value.recipeName!,
+      difficulty: Number(this.addRecipe.value.recipeDifficulty)!,
+      stars: 4,
+      image: "https://picsum.photos/seed/1/400/400"
+    }
+
+  };
 
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}",
+    "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui"),
+  require('flowbite/plugin')],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#fc7035",
+          "secondary": "#f6d860",
+          "accent": "#37cdbe",
+          "neutral": "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  }
 }
 
